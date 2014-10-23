c7c73b2c77b83b307283e295936faf32a59f951c
lombardo <lombardo@mpi-cbg.de>
2014-10-23 18:41:47 +0200

    add a multichannel spot analyzer

---
+++ src/main/java/plugin/trackmate/extra/spotanalyzer/SpotMultiChannelIntensityAnalyzer.java
@@ -0,0 +1,93 @@
+package plugin.trackmate.extra.spotanalyzer;
+
+
+import static plugin.trackmate.extra.spotanalyzer.SpotMultiChannelIntensityAnalyzerFactory.FEATURES;
+//import ij.IJ;
+
+import java.util.Iterator;
+
+import net.imglib2.meta.ImgPlus;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.meta.view.HyperSliceImgPlus;
+//import net.imglib2.util.Util;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.util.SpotNeighborhood;
+import fiji.plugin.trackmate.features.spot.SpotAnalyzer;
+//import fiji.plugin.trackmate.features.spot.SpotAnalyzerFactory;
+
+
+public class SpotMultiChannelIntensityAnalyzer< T extends RealType< T >> implements SpotAnalyzer< T >
+{
+
+	private ImgPlus< T > img;
+	private Model model;
+	private int frame;
+	private String errorMessage;
+	private long processingTime;
+	private int NumChannel;
+	
+	public SpotMultiChannelIntensityAnalyzer( ImgPlus< T > img, final Model model, int frame, int NumChannel )
+	{
+		this.img  = img;
+		this.model = model;	
+		this.frame = frame;
+		this.NumChannel = NumChannel;
+	}
+
+	/*
+	 * PUBLIC METHODS
+	 */
+	
+	
+	@Override
+	public boolean checkInput()
+	{
+		return true;
+	}
+	
+	@Override
+	public boolean process()
+	{	
+		
+		for(int ch=0; ch<NumChannel; ch++)
+		{
+			ImgPlus<T> imgC = HyperSliceImgPlus.fixChannelAxis( img, ch );
+			Iterator<Spot> spots = model.getSpots().iterator(frame, false);
+			
+			while(spots.hasNext() )
+			{
+				Spot spot = spots.next();
+				SpotNeighborhood< T > neighborhood = new SpotNeighborhood< T >( spot, imgC );
+				double mean = 0;
+				
+				for ( T pixel : neighborhood )
+					mean += pixel.getRealDouble();
+				
+				mean /= neighborhood.size();
+				spot.putFeature( FEATURES.get(ch), mean );
+			}
+		}
+
+		
+		//IJ.log("all ok t=" + frame);
+		return true;
+	}
+	
+	
+	@Override
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+
+	@Override
+	public long getProcessingTime()
+	{
+		return processingTime;
+	}
+	
+	
+	
+}
---
+++ src/main/java/plugin/trackmate/extra/spotanalyzer/SpotMultiChannelIntensityAnalyzerFactory.java
@@ -0,0 +1,141 @@
+package plugin.trackmate.extra.spotanalyzer;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import net.imglib2.meta.Axes;
+import net.imglib2.meta.ImgPlus;
+import net.imglib2.meta.view.HyperSliceImgPlus;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+import org.scijava.plugin.Plugin;
+
+import fiji.plugin.trackmate.Dimension;
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.features.spot.SpotAnalyzerFactory;
+
+//import ij.IJ;
+
+@Plugin( type = SpotAnalyzerFactory.class, priority = 0d )
+public class SpotMultiChannelIntensityAnalyzerFactory< T extends RealType< T > & NativeType< T >> implements SpotAnalyzerFactory< T >
+{
+
+	/*
+	 * CONSTANTS
+	 */
+
+	
+	public static final String KEY = "Spot intensity per channel";
+	public static final int nlocal_feat = 10;
+
+	public static final ArrayList< String > FEATURES = new ArrayList< String >( nlocal_feat );
+
+	public static final HashMap< String, String > FEATURE_NAMES = new HashMap< String, String >( nlocal_feat );
+
+	public static final HashMap< String, String > FEATURE_SHORT_NAMES = new HashMap< String, String >( nlocal_feat );
+
+	public static final HashMap< String, Dimension > FEATURE_DIMENSIONS = new HashMap< String, Dimension >( nlocal_feat );
+
+	public static final Map< String, Boolean > IS_INT = new HashMap< String, Boolean >( nlocal_feat );
+	static
+	{
+		for(int i=0; i<nlocal_feat; i++)
+		{	
+			FEATURES.add( 				"MEAN_INTENSITY" + String.format("%02d", i+1) );
+			FEATURE_NAMES.put( 			FEATURES.get(i), "Mean intensity channel " + String.format("%02d", i+1)  );
+			FEATURE_SHORT_NAMES.put( 	FEATURES.get(i), "Mean Ch" + String.format("%02d", i+1) );
+			FEATURE_DIMENSIONS.put( 	FEATURES.get(i), Dimension.INTENSITY );
+			IS_INT.put( 				FEATURES.get(i), Boolean.FALSE );
+		}
+		
+	}
+
+	/*
+	 * METHODS
+	 */
+
+	@Override
+	public SpotMultiChannelIntensityAnalyzer< T > getAnalyzer( final Model model, final ImgPlus< T > img, final int frame, final int channel )
+	{
+		
+		final ImgPlus< T > imgT = HyperSliceImgPlus.fixTimeAxis( img, frame );
+
+		// determine the number of channel
+		long[] dimensions = new long[ img.numDimensions() ]; 
+		img.dimensions(dimensions);
+		int ch_dim = img.dimensionIndex(Axes.CHANNEL);
+		int nCh=1;
+		if (ch_dim>=0)
+			nCh = (int)dimensions[ch_dim];
+		//IJ.log("ch_dim "+ch_dim+" ; n_ch "+nCh);
+		
+		return new SpotMultiChannelIntensityAnalyzer< T >( imgT, model, frame, nCh );
+	}
+
+	@Override
+	public String getKey()
+	{
+		return KEY;
+	}
+
+	@Override
+	public List< String > getFeatures()
+	{
+		return FEATURES;
+	}
+
+	@Override
+	public Map< String, String > getFeatureShortNames()
+	{
+		return FEATURE_SHORT_NAMES;
+	}
+
+	@Override
+	public Map< String, String > getFeatureNames()
+	{
+		return FEATURE_NAMES;
+	}
+
+	@Override
+	public Map< String, Dimension > getFeatureDimensions()
+	{
+		return FEATURE_DIMENSIONS;
+	}
+
+	@Override
+	public String getInfoText()
+	{
+		return null;
+	}
+
+	@Override
+	public ImageIcon getIcon()
+	{
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		return KEY;
+	}
+
+	@Override
+	public Map< String, Boolean > getIsIntFeature()
+	{
+		return IS_INT;
+	}
+
+	@Override
+	public boolean isManualFeature()
+	{
+		return false;
+	}
+
+}
